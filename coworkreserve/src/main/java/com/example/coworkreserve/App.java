f7e0d99f3e19c3434aeb16c91c20c67726a35d3c
PabloHuerres01 <pablomartinezhuerres2001@gmail.com>
2024-12-19 00:03:13 +0100

    V.0

---
+++ coworkreserve/src/main/java/com/example/coworkreserve/App.java
@@ -0,0 +1,12 @@
+package com.example.coworkreserve;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo");
+    }
+}
