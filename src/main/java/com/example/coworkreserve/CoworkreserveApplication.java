775d7ca0709b7fdee8349521f963ae7f691a4426
PabloHuerres01 <pablomartinezhuerres2001@gmail.com>
2024-12-19 00:28:27 +0100

    V.0

---
+++ src/main/java/com/example/coworkreserve/CoworkreserveApplication.java
@@ -0,0 +1,14 @@
+package com.example.coworkreserve;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoworkreserveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoworkreserveApplication.class, args);
+		System.out.println("Hola mundo");
+	}
+
+}
---
+++ src/test/java/com/example/coworkreserve/CoworkreserveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.coworkreserve;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoworkreserveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
